e706cc29b21bd83b0fd37e9837e2e755939a5678
Sergey Karpukhin <telegram.skdvp@gmail.com>
2020-12-26 23:39:14 +0300

    I spring_introduction 9. Внедрение строк и других значений

--- src/main/java/spring_introduction/Person.java
+++ src/main/java/spring_introduction/Person.java
@@ -2,7 +2,26 @@ package spring_introduction;
 
 public class Person {
     private Pet pet;
+    private String surname;
+    private String age;
 
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        System.out.println("Class Person: set surname");
+        this.surname = surname;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        System.out.println("Class Person: set age");
+        this.age = age;
+    }
 
     public Person() {
         System.out.println("Person bean is created");
