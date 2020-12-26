7675b8e83f4a164b7a56732823335f6d49a4c15c
Sergey Karpukhin <telegram.skdvp@gmail.com>
2020-12-26 21:24:14 +0300

    I spring_introduction 7. Dependency Injection с помощью конструктора

--- src/main/java/spring_introduction/Test3.java
+++ src/main/java/spring_introduction/Test3.java
@@ -7,8 +7,8 @@ public class Test3 {
 
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext.xml");
-        Pet pet = context.getBean("myPet", Pet.class);
-        Person person = new Person(pet);
+        Person person = context.getBean("myPerson", Person.class);
         person.callYourPet();
+        context.close();
     }
 }
