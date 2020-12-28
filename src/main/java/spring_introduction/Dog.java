1759edfc77e98cdd0044fa6f14ec53117af5c5f4
Sergey Karpukhin <telegram.skdvp@gmail.com>
2020-12-28 20:18:17 +0300

    I spring_introduction 13. Методы init и destroy

--- src/main/java/spring_introduction/Dog.java
+++ src/main/java/spring_introduction/Dog.java
@@ -3,14 +3,6 @@ package spring_introduction;
 public class Dog implements Pet{
     private String name;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public Dog() {
         System.out.println("Dog bean is created");
     }
@@ -19,4 +11,13 @@ public class Dog implements Pet{
     public void say(){
         System.out.println("Bow-wow");
     }
+
+    protected void init(){
+        System.out.println("Class Dog: init method");
+    }
+
+    private void destroy(){
+        System.out.println("Class Dog: destroy method");
+    }
+
 }
--- src/main/java/spring_introduction/Test5.java
+++ src/main/java/spring_introduction/Test5.java
@@ -2,10 +2,16 @@ package spring_introduction;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class Test4 {
+public class Test5 {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext2.xml");
+
+        Dog myDog = context.getBean("myPet",Dog.class);
+        myDog.say();
+
+        Dog yourDog = context.getBean("myPet",Dog.class);
+        yourDog.say();
 
         context.close();
     }
