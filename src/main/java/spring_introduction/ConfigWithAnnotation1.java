078dcab3310e8196cfd09826d99e08308da6c658
Sergey Karpukhin <telegram.skdvp@gmail.com>
2020-12-28 23:23:43 +0300

    I spring_introduction 15. Аннотация @Autowired для конструктора

--- src/main/java/spring_introduction/ConfigWithAnnotation1.java
+++ src/main/java/spring_introduction/ConfigWithAnnotation1.java
@@ -6,9 +6,9 @@ public class ConfigWithAnnotation1 {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext3.xml");
+        Person person = context.getBean("personBean", Person.class);
+        person.callYourPet();
 
-        Cat myCat = context.getBean("cat", Cat.class);
-        myCat.say();
 
         context.close();
     }
--- src/main/java/spring_introduction/Person.java
+++ src/main/java/spring_introduction/Person.java
@@ -1,5 +1,10 @@
 package spring_introduction;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component("personBean")
 public class Person {
     private Pet pet;
     private String surname;
@@ -23,10 +28,7 @@ public class Person {
         this.age = age;
     }
 
-    public Person() {
-        System.out.println("Person bean is created");
-    }
-
+    @Autowired // с 4.3 : имеется всего один конструктор, то можно не писать
     public Person(Pet pet) {
         System.out.println("Person bean is created");
         this.pet = pet;
