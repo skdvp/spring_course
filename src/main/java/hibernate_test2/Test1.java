0061265f2e0e2fd2947d4ba4cd2cda434cf3012f
Sergey Karpukhin <telegram.skdvp@gmail.com>
2020-12-29 14:22:30 +0300

    III-spring_course-Hibernate 47. Отношение Один-к-Одному (Uni-directional). Часть 1

--- src/main/java/hibernate_test2/Test1.java
+++ src/main/java/hibernate_test2/Test1.java
@@ -1,11 +1,11 @@
-package hibernate_test.entity;
+package hibernate_test2;
 
+import hibernate_test.entity.Employee;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class Test1 {
-/*
     public static void main(String[] args) {
         SessionFactory factory = new Configuration()
                 .configure("hibernate.cfg.xml")
@@ -23,5 +23,4 @@ public class Test1 {
         }
 
     }
-*/
 }
