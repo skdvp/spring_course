293306a9b34a299246ad8b60a953006a5945fafe
Sergey Karpukhin <telegram.skdvp@gmail.com>
2020-12-29 11:14:08 +0300

    III-spring_course-Hibernate 43. Получение Java объектов из БД - part1 - save() & get()

--- src/main/java/hibernate_test/entity/Test2.java
+++ src/main/java/hibernate_test/entity/Test2.java
@@ -4,7 +4,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class Test1 {
+public class Test2 {
     public static void main(String[] args) {
         SessionFactory factory = new Configuration()
                 .configure("hibernate.cfg.xml")
@@ -13,10 +13,15 @@ public class Test1 {
 
         try {
             Session session = factory.getCurrentSession();
-            Employee emp = new Employee("Aleksandr", "Ivanov", "IT", 600);
+            Employee emp = new Employee("Oleg", "Sidorov", "HR", 700);
             session.beginTransaction();
             session.save(emp);
+
+            int myId = emp.getId();
+            Employee employee = session.get(Employee.class, myId);
             session.getTransaction().commit();
+            System.out.println(employee);
+
         } finally {
             factory.close();
         }
