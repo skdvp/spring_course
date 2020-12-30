177594797ce6dec21c6358f0f11048e56486540d
Sergey Karpukhin <telegram.skdvp@gmail.com>
2020-12-30 15:28:56 +0300

    III-spring_course-Hibernate 50. Отношение Один-ко-Многим (Bi-directional) - Part 1 - добавление двух сотрудников и одного департамента

---
+++ src/main/java/hibernate_one_to_many_bi/Test1.java
@@ -0,0 +1,39 @@
+package hibernate_one_to_many_bi;
+
+import hibernate_one_to_many_bi.entity.Department;
+import hibernate_one_to_many_bi.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Test1 {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .addAnnotatedClass(Department.class)
+                .buildSessionFactory();
+        Session session = null;
+        try {
+            session = factory.getCurrentSession();
+
+            Department dep = new Department("IT",300,1200);
+            Employee emp1 = new Employee("Zaur", "Tregulov",800);
+            Employee emp2 = new Employee("Elena", "Smirnova",1000);
+
+            dep.addEmployeeToDepartment(emp1);
+            dep.addEmployeeToDepartment(emp2);
+
+            session.beginTransaction();
+            session.save(dep);
+
+
+            session.getTransaction().commit();
+
+        } finally {
+            session.close();
+            factory.close();
+        }
+
+    }
+}
